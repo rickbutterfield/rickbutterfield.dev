ea7cdf42d8fbc150f8429b8c186f7fbc8dab6191
Rick Butterfield <rick.butterfield@gmail.com>
2023-01-31 09:45:54 +0000

    Fix sorting

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -3,10 +3,10 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('blog').sort(
+	const posts = (await getCollection('blog')).sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
   );
-  
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
