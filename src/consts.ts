b05dd842100d071e8868b95bf818beac2ac90fd7
Rick Butterfield <rick.butterfield@gmail.com>
2023-04-10 12:09:25 +0100

    Add transparent header

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Rick Butterfield';
+export const SITE_TITLE_MOBILE = 'RB';
 export const SITE_DESCRIPTION = 'Full Stack Lead Software Engineer &amp; Event Speaker';
