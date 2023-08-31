e451199c668f7c0d07f7273f37d6a6c998c3337a
Rick Butterfield <rick.butterfield@nexergroup.com>
2023-08-31 15:32:27 +0100

    Upgrade Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
@@ -16,5 +13,5 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rickbutterfield.dev',
-  integrations: [mdx(), sitemap(), image(), prefetch(), robotsTxt()]
+  integrations: [mdx(), sitemap(), prefetch(), robotsTxt()]
 });
