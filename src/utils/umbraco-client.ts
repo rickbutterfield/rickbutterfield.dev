e1bd20253bfc00b6bc8c77b8b120ccb45028bee4
Rick Butterfield <rick.butterfield@nexergroup.com>
2023-11-04 12:26:06 +0000

    Connect to Umbraco Content Delivery API

---
+++ src/utils/umbraco-client.ts
@@ -0,0 +1,65 @@
+type Options = {
+  sort?: {
+    type: 'createDate' | 'updateDate' | 'sortOrder' | 'name' | 'level'
+    order: 'asc' | 'desc'
+  }
+  expand?: 'all' | string[]
+}
+
+function getSortParam(sort: Options['sort']) {
+  if (!sort) return ''
+
+  return `&sort=${sort.type}:${sort.order}`
+}
+
+function getExpandParam(expand: Options['expand']): string {
+  let expandParam = ''
+
+  if (expand === 'all') {
+    expandParam = '&expand=all'
+  } else if (typeof expand === 'object') {
+    expandParam = '?expand=property:'
+
+    expand.forEach((item, index) => {
+      expandParam += index === 0 ? item : `,${item}`
+    })
+  }
+
+  return expandParam
+}
+
+// Client used to query our Umbraco CMS
+function createClient({ domain }: { domain: string }) {
+  const apiUrl = `${domain}/umbraco/delivery/api/v1/content`
+
+  return {
+    getContentById: async (id: string) => {
+      const response = await fetch(`${apiUrl}/item/${id}`, {
+        headers: new Headers({
+          'ngrok-skip-browser-warning': '1' // Remove ngrok warning to allow deployment build to pass
+        })
+      })
+      const data = await response.json()
+
+      return data
+    },
+    getContentByType: async (itemType: string, options?: Options) => {
+      const { sort, expand } = options || {}
+
+      // Example: https://localhost:44326/umbraco/delivery/api/v1/content?filter=contentType:blogPost&sort=createDate:desc&expand=property:content
+      const response = await fetch(
+        `${apiUrl}?filter=contentType:${itemType}${getSortParam(sort)}${getExpandParam(expand)}`,
+        {
+          headers: new Headers({
+            'ngrok-skip-browser-warning': '1' // Remove ngrok warning to allow deployment build to pass
+          })
+        }
+      )
+      const data = await response.json()
+
+      return data.items
+    }
+  }
+}
+
+export default createClient({ domain: import.meta.env.PUBLIC_BASE_URL })
