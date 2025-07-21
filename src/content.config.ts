158c0dba9dd2e47ae410d80d4d5676ae1bcc95da
Rick Butterfield <rick.butterfield@gmail.com>
2025-07-21 09:13:58 +0100

    Update all pages to use content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from 'astro:content';
-import { ContentService, OpenAPI, type BlogPostContentModel } from './api';
+import { ContentService, OpenAPI, type BlogPostContentModel, type ProjectPostContentModel, type SpeakingPostContentModel } from './api';
 import type { ZodType } from 'astro/zod';
 
 OpenAPI.BASE = import.meta.env.PUBLIC_BASE_URL;
@@ -26,4 +26,49 @@ const blog = defineCollection({
   })
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  loader: async() => {
+    const response = await ContentService.queryV20({
+      filter: ["contentType:projectPost"],
+      sort: ["sortOrder:asc"],
+    });
+
+    return response.items.map((item: ProjectPostContentModel) => ({
+      id: item.id,
+      name: item.name,
+      slug: item.route.path,
+      content: item,
+    }));
+  },
+  schema: z.object({
+    id: z.string(),
+    slug: z.string(),
+    name: z.string(),
+    content: z.any() as ZodType<ProjectPostContentModel>,
+  })
+});
+
+const speaking = defineCollection({
+  loader: async() => {
+    const response = await ContentService.queryV20({
+      filter: ["contentType:speakingPost"],
+      sort: ["eventDate:desc"],
+      expand: 'properties[$all[properties[featuredImage]]]',
+    });
+
+    return response.items.map((item: SpeakingPostContentModel) => ({
+      id: item.id,
+      name: item.name,
+      slug: item.route.path,
+      content: item,
+    }));
+  },
+  schema: z.object({
+    id: z.string(),
+    slug: z.string(),
+    name: z.string(),
+    content: z.any() as ZodType<SpeakingPostContentModel>,
+  })
+});
+
+export const collections = { blog, projects, speaking };
